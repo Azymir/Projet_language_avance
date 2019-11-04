ab85b70edc057da31452bf8264a9adac725ae6df
Hugo Tedeschi <hugo.tedeschi@ens.uvsq.fr>
2019-11-04 09:33:45 +0100

    Modification de tableau de bord, selon moi il y a 3 element par type objet ( 3 vannes, 6 pompes etc)

--- Fichier.hh/Tableau_bord.hh
+++ Fichier.hh/Tableau_bord.hh
@@ -4,10 +4,10 @@
 class TableauBord
 {
 	private:
-		Vanne V;						// etat du Vanne 			(ouverture et fermeture des vannes)
-		Pompe P;						// etat de la pompe		(démarrage et arrêt des pompes)
-		Reservoir R;				// etat du Reservoir	(Reservoir vide et plein)
-		Moteur M;						// etat du Moteur 		(Moteur en marche ou en panne)
+		Vanne V1,V2,V3;						// etat du Vanne 			(ouverture et fermeture des vannes)
+		Pompe P1,P2,P3, S1, S2, S3;		    // etat de la pompe		(démarrage et arrêt des pompes)
+		Reservoir R1, R1, R2;				// etat du Reservoir	(Reservoir vide et plein)
+		Moteur M1, M2, M3;						// etat du Moteur 		(Moteur en marche ou en panne)
 
 
 
